71cb5e2168eb17d0fd5df0c175ddd118b0404b87
Sam Gunaratne <sgunaratne@pivotal.io>
2018-03-26 15:16:51 +0100

    Add validation of environment variables
    Signed-off-by: Niki Maslarski <nikolay.maslarski@sap.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -17,6 +18,24 @@ func main() {
 		port = "8080"
 	}
 
+	parseRequiredEnv("SERVICE_ACCOUNT_JSON")
+	brokerURL := parseRequiredEnv("BROKER_URL")
+	parseRequiredEnv("USERNAME")
+	parseRequiredEnv("PASSWORD")
+
+	_, err := url.ParseRequestURI(brokerURL)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("BROKER_URL must be a valid URL: %s", brokerURL))
+	}
+
 	fmt.Printf("About to listen on port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func parseRequiredEnv(env string) string {
+	parsedEnv := os.Getenv(env)
+	if parsedEnv == "" {
+		log.Fatal(fmt.Sprintf("Missing %s environment variable", env))
+	}
+	return parsedEnv
 }
--- main_test.go
+++ main_test.go
@@ -33,10 +33,10 @@ var _ = Describe("GCP Broker Proxy", func() {
 		session.Kill()
 	})
 
-	Context("when the server is correctly configured", func() {
+	Describe("when the server is correctly configured", func() {
 		BeforeEach(func() {
 			port = strconv.Itoa(8081 + config.GinkgoConfig.ParallelNode)
-			envs = []string{"PORT=" + port}
+			envs = []string{"PORT=" + port, "SERVICE_ACCOUNT_JSON={}", "BROKER_URL=https://example.org", "USERNAME=admin", "PASSWORD=foo"}
 		})
 
 		It("logs that the server is about to start on a specific port", func() {
@@ -49,11 +49,83 @@ var _ = Describe("GCP Broker Proxy", func() {
 
 		Context("when no port is specified", func() {
 			BeforeEach(func() {
-				envs = []string{}
+				envs = []string{"SERVICE_ACCOUNT_JSON={}", "BROKER_URL=https://example.org", "USERNAME=admin", "PASSWORD=foo"}
 			})
 
 			It("it starts on the default port of 8080", func() {
 				Eventually(session).Should(Say("About to listen on port 8080"))
+			})
+		})
+	})
+
+	Describe("when the server is not correctly configured", func() {
+		Context("when the server has not been provided service account information", func() {
+			BeforeEach(func() {
+				envs = []string{"BROKER_URL=https://example.org", "USERNAME=admin", "PASSWORD=foo"}
+			})
+
+			It("it fails to start", func() {
+				Eventually(session).Should(gexec.Exit())
+			})
+
+			It("logs that the server requires the SERVICE_ACCOUNT_JSON param", func() {
+				Eventually(session.Err).Should(Say("Missing SERVICE_ACCOUNT_JSON environment variable"))
+			})
+		})
+
+		Context("when the server has not been provided broker url", func() {
+			BeforeEach(func() {
+				envs = []string{"SERVICE_ACCOUNT_JSON={}", "USERNAME=admin", "PASSWORD=foo"}
+			})
+
+			It("it fails to start", func() {
+				Eventually(session).Should(gexec.Exit())
+			})
+
+			It("logs that the server requires the BROKER_URL param", func() {
+				Eventually(session.Err).Should(Say("Missing BROKER_URL environment variable"))
+			})
+		})
+
+		Context("when the broker url is invalid", func() {
+			BeforeEach(func() {
+				envs = []string{"SERVICE_ACCOUNT_JSON={}", "BROKER_URL=notaurl", "USERNAME=admin", "PASSWORD=foo"}
+			})
+
+			It("it fails to start", func() {
+				Eventually(session).Should(gexec.Exit())
+			})
+
+			It("logs that the server requires the BROKER_URL param", func() {
+				Eventually(session.Err).Should(Say("BROKER_URL must be a valid URL: notaurl"))
+			})
+		})
+
+		Context("when the server has not been provided username", func() {
+			BeforeEach(func() {
+				envs = []string{"SERVICE_ACCOUNT_JSON={}", "BROKER_URL=https://example.org", "PASSWORD=foo"}
+			})
+
+			It("it fails to start", func() {
+				Eventually(session).Should(gexec.Exit())
+			})
+
+			It("logs that the server requires the USERNAME param", func() {
+				Eventually(session.Err).Should(Say("Missing USERNAME environment variable"))
+			})
+		})
+
+		Context("when the server has not been provided password", func() {
+			BeforeEach(func() {
+				envs = []string{"SERVICE_ACCOUNT_JSON={}", "BROKER_URL=https://example.org", "USERNAME=admin"}
+			})
+
+			It("it fails to start", func() {
+				Eventually(session).Should(gexec.Exit())
+			})
+
+			It("logs that the server requires the PASSWORD param", func() {
+				Eventually(session.Err).Should(Say("Missing PASSWORD environment variable"))
 			})
 		})
 	})
